7a0f06f202f028ea570856e3b5e8b78d4e89fd34
bayujunisp <bayupribadi37@gmail.com>
2022-11-28 11:00:33 +0700

    push htaccess

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8080/sp_tomato/';
+$config['base_url'] = 'http://localhost/sp_tomato/';
 
 /*
 |--------------------------------------------------------------------------
